e7c2093969ebd21e90761162d4117aec2c1b4ce9
adsseno <senojiahanadon@gmail.com>
2024-09-10 14:23:53 -0700

    Completed Activity 3: Eloquent ORM Basics

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,45 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/students/create', function() {
+$student = new Student();
+$student ->first_name ='john';
+$student ->last_name ='Doe';
+$student ->email ='johndoe@gmail.com';
+$student ->age =22;
+$student ->save();
+return 'Student Created';
+});
+
+Route::get('/students', function() {
+	$students = Student::all();
+	return $students;
+
+});
+
+Route::get('/students/update', function() {
+	$student = Student::where('email', 'johndoe@gmail.com')->firts();
+	$student ->email ='johndoe@gmail';
+	$student ->age =23; //update age as well
+	$student ->save();
+	return 'Students update!';
+});
+
+Route::get('/students/delete', function() {
+	$student = Student::where('email', 'johndoe@gmail.com')->firts();
+	$student ->delete();
+	return 'Students Delete!';
+});
+
+Route::get('/courses/create', function() {
+	$course = new Course();
+	$course->course_name = 'Introduction to Databases';
+	$course->save();
+	return 'Course Created';
+});
+
+Route::get('/course/{id}/students', function($id) {
+	$course =Course::find($id);
+	return $course->students;
 });
